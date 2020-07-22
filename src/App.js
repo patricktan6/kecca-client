5a755f9535b7ec93a3bc32de35e4f0ddefe5ab72
Lye Yi Xian <lyeyx999@hotmail.com>
2020-07-22 21:44:26 +0800

    Added MuiTheme to the website

--- src/App.js
+++ src/App.js
@@ -10,20 +10,34 @@ import Signup from "./pages/signup";
 // Components
 import Navbar from "./components/Navbar";
 
+// Material-UI
+import MuiThemeProvider from "@material-ui/core/styles/MuiThemeProvider";
+import createMuiTheme from "@material-ui/core/styles/createMuiTheme";
+import { blue } from "@material-ui/core/colors";
+
+const theme = createMuiTheme({
+  palette: {
+    primary: blue,
+    secondary: blue,
+  },
+});
+
 function App() {
   return (
-    <div className="App">
-      <Router>
-        <Navbar />
-        <div className="container">
-          <Switch>
-            <Route exact path="/" component={Home} />
-            <Route exact path="/login" component={Login} />
-            <Route exact path="/signup" component={Signup} />
-          </Switch>
-        </div>
-      </Router>
-    </div>
+    <MuiThemeProvider theme={theme}>
+      <div className="App">
+        <Router>
+          <Navbar />
+          <div className="container">
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route exact path="/login" component={Login} />
+              <Route exact path="/signup" component={Signup} />
+            </Switch>
+          </div>
+        </Router>
+      </div>
+    </MuiThemeProvider>
   );
 }
 
