0219856f85cc57f1c5d1f80529e6d21354e9e5ac
Tan Xi Zhe <tanxizhe@gmail.com>
2020-07-25 18:28:08 +0800

    remove home when logged in and added logout

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,29 +1,63 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
 import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
 
 // Material-ui
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Button from "@material-ui/core/Button";
 
+// Redux
+import { connect } from "react-redux";
+import { logoutUser } from "../redux/actions/userActions";
+
 class Navbar extends Component {
   render() {
+
+    const { authenticated } = this.props;
+
+    const handleLogout = () => {
+      this.props.logoutUser();
+    }
+
     return (
       <AppBar>
         <Toolbar className="nav-container">
-          <Button color="inherit" component={Link} to="/login">
-            Login
-          </Button>
-          <Button color="inherit" component={Link} to="/">
-            Home
-          </Button>
-          <Button color="inherit" component={Link} to="/signup">
-            Signup
-          </Button>
+          {authenticated ? (
+            <Fragment>
+              <Button color="inherit" component={Link} to="/">
+                Home
+              </Button>
+              <Button color="inherit" component={Link} to="/login" onClick={handleLogout} >
+                Logout
+              </Button>
+            </Fragment>
+          ) : (
+            <Fragment>
+              <Button color="inherit" component={Link} to="/login">
+                Login
+              </Button>
+              <Button color="inherit" component={Link} to="/signup">
+                Signup
+              </Button>
+            </Fragment>
+          )}
         </Toolbar>
       </AppBar>
     );
   }
 }
 
-export default Navbar;
+Navbar.propTypes = {
+  authenticated: PropTypes.bool.isRequired,
+  logoutUser: PropTypes.func.isRequired,
+}
+
+const mapStateToProps = (state) => ({
+  user: state.user,
+  authenticated: state.user.authenticated,
+});
+
+const mapActionsToProps = { logoutUser };
+
+export default connect(mapStateToProps, mapActionsToProps)(Navbar);
