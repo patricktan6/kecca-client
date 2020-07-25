d6ef0b944d70ee5b89b44b7bf8ab3da3ad1431f9
Lye Yi Xian <lyeyx999@hotmail.com>
2020-07-25 20:21:44 +0800

    Fixed admin profile (can retrieve now)

--- src/redux/actions/userActions.js
+++ src/redux/actions/userActions.js
@@ -32,15 +32,28 @@ export const loginUser = (userData, history) => (dispatch) => {
 
 export const getUserData = () => (dispatch) => {
   dispatch({ type: LOADING_USER });
-  axios
-    .get("/user")
-    .then((res) => {
-      dispatch({
-        type: SET_USER,
-        payload: res.data,
-      });
-    })
-    .catch((err) => console.log(err));
+  const startingHeader = localStorage.FBIdToken.split(" ")[0];
+  if (startingHeader === "User") {
+    axios
+      .get("/user")
+      .then((res) => {
+        dispatch({
+          type: SET_USER,
+          payload: res.data,
+        });
+      })
+      .catch((err) => console.log(err));
+  } else {
+    axios
+      .get("/admin")
+      .then((res) => {
+        dispatch({
+          type: SET_USER,
+          payload: res.data,
+        });
+      })
+      .catch((err) => console.log(err));
+  }
 };
 
 export const signupUser = (newUserData, history) => (dispatch) => {
@@ -48,10 +61,13 @@ export const signupUser = (newUserData, history) => (dispatch) => {
   axios
     .post("/signup", newUserData)
     .then((res) => {
-      setAuthorizationHeader(res.data.token);
-      dispatch(getUserData());
-      dispatch({ type: CLEAR_ERRORS });
-      history.push("/");
+      setAuthorizationHeader(res.data.token)
+        .then(() => {
+          dispatch(getUserData());
+          dispatch({ type: CLEAR_ERRORS });
+          history.push("/");
+        })
+        .catch((err) => console.log(err));
     })
     .catch((err) => {
       dispatch({
@@ -64,9 +80,11 @@ export const signupUser = (newUserData, history) => (dispatch) => {
 const setAuthorizationHeader = (token) => {
   const decodedToken = jwtDecode(token);
   const userId = decodedToken.user_id;
+
   return axios.get(`/user/${userId}`).then((res) => {
     const startingHeader = res.data.adminStatus.tokenHeader;
     const FBIdToken = `${startingHeader}${token}`;
+
     localStorage.setItem("FBIdToken", FBIdToken);
     axios.defaults.headers.common["Authorization"] = FBIdToken;
   });
